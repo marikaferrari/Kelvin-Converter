3fbdb4c60d6ed4e2bfc57036f4232d1d1d1e6434
francesco44Hello <francescolongo44@gmail.com>
2023-08-01 19:44:03 +0100

    intro on js

---
+++ index.js
@@ -0,0 +1,22 @@
+// const kelvin = 293; //Returns K
+// const celsius = kelvin - 273; //Returns 20°C
+// const fahrenheit = celsius * (9/5) + 32; //Returns temperature in Fahreinheit
+// Math.floor(fahrenheit); //Rounds down Fahreinheit temperature to decimal number
+// console.log(`'The temperature is ${fahrenheit} degrees Fahreinheit'`); //Prints temperature in Fahreinheit
+// const newton = celsius * (33/100); //Returns temperature in Netwon
+// Math.floor(newton); //Rounds down temperature in Newton to decimal number
+// console.log(`'The temperature is currently ${newton}' Newton.`); //Prints temperature in N
+// console.log(`'The temperature is ${celsius} in Celsius'`);
+
+// let input = document.getElementById('#input')
+let button = document.getElementById('button').addEventListener("click", test);
+
+
+
+function test (){
+   getElementById('label').textContent = 'suca';
+}
+
+// function getInput(e){
+    
+// }
