b16ca6d66ee239d75ac8e698ca31088139ad2bcb
Marika <ferrari.marika@gmail.com>
2023-08-25 16:51:52 +0100

    working js

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-// const kelvin = 293; //Returns K
-// const celsius = kelvin - 273; //Returns 20°C
-// const fahrenheit = celsius * (9/5) + 32; //Returns temperature in Fahreinheit
-// Math.floor(fahrenheit); //Rounds down Fahreinheit temperature to decimal number
-// console.log(`'The temperature is ${fahrenheit} degrees Fahreinheit'`); //Prints temperature in Fahreinheit
-// const newton = celsius * (33/100); //Returns temperature in Netwon
-// Math.floor(newton); //Rounds down temperature in Newton to decimal number
-// console.log(`'The temperature is currently ${newton}' Newton.`); //Prints temperature in N
-// console.log(`'The temperature is ${celsius} in Celsius'`);
+/* Per convertire la temperatura da Kelvin a Celsius:
+1. Creare una costante per l'input;
+2. Creare una costante per l'output;
+3. Creare una funzione che faccia il calcolo e che arrotondi il numero;
+4. 
+*/
 
-// let input = document.getElementById('#input')
-let button = document.getElementById('button').addEventListener("click", test);
+const kelvinInput = document.querySelector("#input"); //constante per l'input
 
+const celsiusOutput = document.querySelector("#celsius"); //costante per l'output
 
-
-function test (){
-   getElementById('label').textContent = 'suca';
+function temperature() {
+   const kelvin = parseFloat(kelvinInput.value); /*nella variabile "kelvin", il method "parseFloat" serve per convertire
+   la stringa/testo in un numero, dato che l'input dell'user sara un testo ma a noi serve numero 
+   Invece la proprieta ".value" ti riporta quello che hai inserito come input */
+   const celsius = kelvin - 273.15; 
+   return Math.floor(celsius); //"method Math.floor per arrotondare il numero"
 }
 
-// function getInput(e){
-    
-// }
+function converter() {
+   celsiusOutput.value = temperature();
+} 
+//dichiariamo la funzione che poi viene presa dall'addEventListener
+// senza dichiarare la funzione converter qui per l'addEventListener, si potrebbe fare cosi:
+// document.querySelector("#button").addEventListener("click", () => {}); 
+// RICORDARE la sintessi delle parentesi, ("click", function() {} ) - come creare una funzione normale
+
+document.querySelector("#button").addEventListener("click", converter /*() => /*la parentesi extra non serve qui ma ve messa a fine funzione {
+  ; // usiamo ancora la proprieta .value per modificare il valore dell'elemento del DOM*/)
+
+console.log(temperature());
